474c8aa08966864c8d89da918686f3b5cc189c27
Azhan Hussain <azhan.hussain@metafroliclabs.com>
2024-09-28 02:02:49 +0500

    Font integrate

---
+++ android/app/src/main/assets/custom/index.ts
@@ -0,0 +1,11 @@
+const fonts = {
+  OpenSans: {
+    regular: 'OpenSans-Regular',
+    bold: 'OpenSans-Bold',
+    semiBold: 'OpenSans-SemiBold',
+    light: 'OpenSans-Light',
+    medium: 'OpenSans-Medium',
+  },
+};
+
+export default fonts;
